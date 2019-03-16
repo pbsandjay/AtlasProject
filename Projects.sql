8100dc8c1cd45f8a57686584500387e05b09a205
endoffile78 <endoffile78@yahoo.com>
2019-03-16 13:16:30 -0500

    Update Projects.sql

--- Projects.sql
+++ Projects.sql
@@ -5,9 +5,9 @@
     [Manager]           VARCHAR (50)  NOT NULL,
     [Original_Due_Date] DATE          NULL,
     [Current_Due_Date]  DATE          NULL,
-    [Finished_Date]     DATE          NULL,
+    [Actual_Date]     DATE          NULL,
     [Status]            VARCHAR (20)  NOT NULL,
-    [Type]              VARCHAR (50)  NOT NULL,
+    [Type]              VARCHAR (20)  NOT NULL,
     [Done]              VARCHAR (10)  NULL,
     [Comments] VARCHAR(200) NULL, 
 );
